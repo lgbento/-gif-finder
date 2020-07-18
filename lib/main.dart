ba513a3b2b501e758b0654aaf40f665b1fb1368b
LuisDev <luisgabrielbento@hotmail.com>
2020-07-18 11:28:58 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:buscador_de_gifs/ui/gif_page.dart';
+import 'package:buscador_de_gifs/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+      theme: ThemeData(
+      hintColor: Colors.white,
+      primaryColor: Colors.white,
+      inputDecorationTheme: InputDecorationTheme(
+      enabledBorder:
+      OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+  focusedBorder:
+  OutlineInputBorder(borderSide: BorderSide(color: Colors.blueAccent)),
+  hintStyle: TextStyle(color: Colors.blueAccent),
+  )),
+  ));
+}
+
